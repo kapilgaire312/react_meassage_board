9a8e8cbcbb3afdfcff00f30547fc7e9d83a85e07
Kapil Gaire <Kap@KapilMacBook-Pro.local>
2025-07-03 11:31:27 +0545

    auto-scroll enabled

--- src/components/MessageBoard/Comment.jsx
+++ src/components/MessageBoard/Comment.jsx
@@ -1,10 +1,15 @@
-function Comment({ user, comment }) {
+import { forwardRef } from 'react'
+
+const Comment = forwardRef(({ user, comment }, ref) => {
   return (
-    <div className="bg-gray-500 rounded my-2 mx-1 px-2 max-w-64 break-all">
+    <div className="bg-gray-500 rounded my-2 mx-1 px-2 max-w-64 break-all" ref={ref}>
       <p className="text-xs opacity-80">{user}</p>
       <p className="px-2 ">{comment}</p>
     </div>
+  );
+});
 
-  )
-}
 export default Comment
+
+//If control is needed by the parent → pass the ref down.
+//If control remains local to the child → define the ref there.
--- src/components/MessageBoard/CommentLists.jsx
+++ src/components/MessageBoard/CommentLists.jsx
@@ -1,16 +1,22 @@
 import Comment from './Comment'
 import { CommentsContext } from "./contexts"
-import { useContext } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 function CommentLists() {
   const { comments } = useContext(CommentsContext)
   console.log(comments)
+  const latestCommentRef = useRef();
+
+  useEffect(() => {
+    latestCommentRef?.current?.scrollIntoView()
+  }, [comments])
   return (
     <div className='flex justify-center my-6 '>
-      <div className="bg-cyan-100 rounded text-left h-96 w-64 overflow-y-scroll no-scrollbar scroll-smooth
+      <div className="bg-cyan-100 rounded text-left h-96 w-64 overflow-y-scroll no-scrollbar
 ">
         {comments?.map((item, index) =>
-          <Comment key={index} user={item.user} comment={item.comment} />
+          <Comment key={index} user={item.user} comment={item.comment} ref={latestCommentRef} />
         )}
+
 
       </div>
 
